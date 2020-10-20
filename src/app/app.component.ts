4d0e542f107f52163095cf4c68d4e70629064a85
Roopam Paradkar <roopamparadkar@gmail.com>
2020-10-21 02:53:25 +0530

    complete single panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+
   title = 'wheatherex';
+  weatherExist = false;
+  noCity = false;
+  cityName = new FormControl('');
+
+  weatherData = {};
+  weatherIcon: string;
+  iconUrl: string;
+  localData = {};
+  getWeatherSubscription: Subscription;
+
+  constructor(private httpClient: HttpClient) { }
+
+
+  getWeather() {
+    this.getWeatherSubscription = timer(0, 5000)
+      .subscribe(() => {
+        this.httpClient.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.cityName.value}&units=metric&appid=082b6b4542f8165adcc1deae4ebe2258`)
+          .subscribe(
+            (result: any) => {
+              this.weatherData = result;
+              const weatherData = result;
+              const weatherIcon = weatherData.weather[0].icon
+              this.iconUrl = `http://openweathermap.org/img/wn/${weatherIcon}.png`
+              this.weatherExist = true;
+              localStorage.setItem('weatherData', JSON.stringify(this.weatherData));
+              localStorage.setItem('iconUrl', this.iconUrl);
+            },
+            error => {
+              this.noCity = true;
+              this.cityName.setErrors({ valid: false });
+              console.error(error)
+            })
+      })
+  }
+
+  updateWeather() {
+    this.noCity = false;
+    this.weatherExist = false;
+    localStorage.clear();
+    this.getWeatherSubscription.unsubscribe();
+  }
+
+  checkWeatherConditions(weather) {
+    if (weather == 'Rain') {
+      return 'rainy';
+    }
+    else if (weather == 'Clouds') {
+      return 'cloudy';
+    }
+    else if (weather == 'Sunny') {
+      return 'sunny';
+    }
+    else {
+      return 'sunny';
+    }
+
+  }
+  ngOnInit(): void {
+    if (localStorage.getItem('weatherData')) {
+      this.weatherExist = true;
+      this.weatherData = JSON.parse(localStorage.getItem('weatherData'));
+      this.iconUrl = localStorage.getItem('iconUrl');
+    }
+    
+  }
+
+  ngOnDestroy(): void {
+    localStorage.clear();
+    this.getWeatherSubscription.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +20,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
